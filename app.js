25d13f73fb4e1f68e08d40c98b06bd9c28f2ed0c
iamkoks <iamkoks@users.noreply.github.com>
2020-09-21 16:53:22 +0300

    connect to mongoDB

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+
+const PORT = config.get('port') || 5000
+
+async function start () {
+    try{
+        await mongoose.connect(config.get('mongoURL'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        app.listen(5000, () => console.log(`Server start on port ${PORT}`))
+    } catch (err) {
+        console.log('Server Error', err.message);
+        process.exit(1)
+    }
+}
+
+start()
