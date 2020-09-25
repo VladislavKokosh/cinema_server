01bf728cc6733c60a785afa951f73f95d0c0c23d
iamkoks <iamkoks@users.noreply.github.com>
2020-09-25 11:23:09 +0300

    Add Model SelectedPlaces

---
+++ models/SelectedPlaces.js
@@ -0,0 +1,16 @@
+const {Schema, model} = require('mongoose')
+
+const selectedPlaceSchema = new Schema({
+    id_session: {type: Schema.ObjectId, ref: "Session"},
+    seats: [{
+        row: Number,
+        place: Number,
+        cost: Number,
+        id_user: {type: Schema.ObjectId, ref: "User"}
+    }]
+},
+{
+    versionKey: false
+})
+
+module.exports = model('SelectedPlace', selectedPlaceSchema)
